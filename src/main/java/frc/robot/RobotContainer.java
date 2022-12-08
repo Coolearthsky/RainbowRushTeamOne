2da9827681ea8d7f99d7e18dc5887204b17f48c6
Joel Truher <joel@truher.org>
2022-12-08 11:29:16 -0800

    uncommented default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -36,7 +36,7 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
-  // armSubsystem.setDefaultCommand(armControl);
+   armSubsystem.setDefaultCommand(armControl);
   
   buttonA.whileHeld(pinch);
   buttonB.whileHeld(unPinch);
